17cd318b97f2c598955bb68e7e0e863b96cf355b
Tyuiu.SheludkovAA@outlook.com <Tyuiu.SheludkovAA@outlook.com>
2023-09-19 20:41:52 +0500

    Task 0 V10

---
+++ Tyuiu.SheludkovAA.Sprint1.Task0.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SheludkovAA.Sprint1.Task0.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SheludkovAA.Sprint1.Task0.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("36758810-a8b6-40a9-a0fa-d705c3b3085f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
